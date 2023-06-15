5d9677656dcf4cd14af234752fca16302fce6808
Cornelius Carl <corneliuscarl@me.com>
2023-06-15 18:24:01 +0200

    feat: added bpy shape creation

--- blender_test.py
+++ blender_test.py
@@ -1,45 +1,60 @@
-import bpy
-import numpy as np
 import random
 
-#pip install fake-bpy-module-2.93
+import bpy
+import numpy as np
+
+# pip install fake-bpy-module-2.93
 # or pip install fake-bpy-module-latest
-#conda install -c kitsune.one python-blender
+# conda install -c kitsune.one python-blender
 
-#safe script changes in blender via 'alt + s'
-
-
-#Helper Functions
-#from https://stackoverflow.com/questions/8487893/generate-all-the-points-on-the-circumference-of-a-circle
-def PointsInCircum(r,z,n=15):
-    return [[np.cos(2*np.pi/n*x)*r,np.sin(2*np.pi/n*x)*r, float(z)] for x in range(0,n+1)]
+# safe script changes in blender via 'alt + s'
 
 
-#Initialize vertices of bottle
-height = 30
-radius = 10
-
-#the mouth
-mouthVertices= [PointsInCircum(3,-1)]
-topVertices= [PointsInCircum(radius,height)]
-
-variableVertices = []
-for level in range(height):
-    nextVertices = PointsInCircum(radius,level)
-    # n = nextVertices[0]
-    # nextVertices.append(n)
-    for i in range(len(nextVertices) - 1):
-        factor = random.random()
-        nextVertices[i][0] *= factor
-        nextVertices[i][1] *= factor
-        if (i == 0):
-            nextVertices[-1][0] *= factor
-            nextVertices[-1][1] *= factor
+# Helper Functions
+# from https://stackoverflow.com/questions/8487893/generate-all-the-points-on-the-circumference-of-a-circle
+def PointsInCircum(r, z, n=15):
+    return [
+        [np.cos(2 * np.pi / n * x) * r, np.sin(2 * np.pi / n * x) * r, float(z)]
+        for x in range(0, n + 1)
+    ]
 
 
-    variableVertices += [nextVertices]
+def createBottle():
+    # Initialize vertices of bottle
+    height = 30
+    radius = 10
 
-allVertices = mouthVertices + variableVertices + topVertices
+    # the mouth
+    mouthVertices = [PointsInCircum(3, -1)]
+    topVertices = [PointsInCircum(radius, height)]
 
+    variableVertices = []
+    for level in range(height):
+        nextVertices = PointsInCircum(radius, level)
+        # n = nextVertices[0]
+        # nextVertices.append(n)
+        for i in range(len(nextVertices) - 1):
+            factor = random.random()
+            nextVertices[i][0] *= factor
+            nextVertices[i][1] *= factor
+            if i == 0:
+                nextVertices[-1][0] *= factor
+                nextVertices[-1][1] *= factor
 
-bpy.ops.mesh.convex_hull(delete_unused=True, use_existing_faces=True, make_holes=False, join_triangles=True, face_threshold=0.698132, shape_threshold=0.698132, uvs=False, vcols=False, seam=False, sharp=False, materials=False)
+        variableVertices += [nextVertices]
+
+    allVertices = mouthVertices + variableVertices + topVertices
+
+    return bpy.ops.mesh.convex_hull(
+        delete_unused=True,
+        use_existing_faces=True,
+        make_holes=False,
+        join_triangles=True,
+        face_threshold=0.698132,
+        shape_threshold=0.698132,
+        uvs=False,
+        vcols=False,
+        seam=False,
+        sharp=False,
+        materials=False,
+    )
