3501b9e5ffbdeb9df1a736914773123d4a9b78c4
Chris Kang <danhwee9@gmail.com>
2023-07-12 03:01:22 +0200

    fix: added parsing

--- rbfopt/soo/json_sorting.py
+++ rbfopt/soo/json_sorting.py
@@ -1,30 +1,69 @@
+# import json
+# import numpy
+
+# rbf = "C:/Users/danhw/Desktop/1_ITECH/3.CIA/A1/hyper-hydration/rbfopt/soo/rbfopt_1.json" 
+# cmaes = "C:/Users/danhw/Desktop/1_ITECH/3.CIA/A1/hyper-hydration/rbfopt/soo/cmaes_1.json"
+# random = "C:/Users/danhw/Desktop/1_ITECH/3.CIA/A1/hyper-hydration/rbfopt/soo/random_1.json"
+
+
+# # Step 1: Read the JSON file and load its contents into a Python object
+# with open(random) as file:
+#     data = json.load(file)
+
+# sorted_data = sorted(data.items(), key=lambda x: x[1]['objectives']['obj'])
+
+# # Step 3: Write the sorted data back to a JSON file
+# # sorted_data_dict = {k: v for k, v in sorted_data}
+
+# # with open(save_path, 'w') as file:
+# #     json.dump(sorted_data_dict, file, indent=4)
+
+
+# a = list(sorted_data)[0][1]["params"]
+# print(list(sorted_data)[0][1]["objectives"]["obj"])
+# params = []
+
+# # print(a)
+# for b in a.values():
+#     params.append(b)
+
+#print(params)
+
+# sorted_data_dict = {k: v for k, v in sorted_data[0]}
+
+# print(sorted_data_dict)
+
+#-----------moo
 import json
 import numpy
 
-file_path = "C:/Users/danhw/Desktop/1_ITECH/3.CIA/A1/hyper-hydration/rbfopt/soo/random_1.json"
-
+moead = "C:/Users/danhw/Desktop/1_ITECH/3.CIA/A1/hyper-hydration/rbfopt/moo/moead_1.json"
+nsga2 = "C:/Users/danhw/Desktop/1_ITECH/3.CIA/A1/hyper-hydration/rbfopt/moo/nsga2_1.json"
+nsga3 = "C:/Users/danhw/Desktop/1_ITECH/3.CIA/A1/hyper-hydration/rbfopt/moo/nsga3_1.json"
 
 # Step 1: Read the JSON file and load its contents into a Python object
-with open(file_path) as file:
+with open(nsga3) as file:
     data = json.load(file)
 
-sorted_data = sorted(data.items(), key=lambda x: x[1]['objectives']['obj'])
+def sorting_function(item):
+    objectives = item['objectives']
+    time = objectives['time']
+    opening = objectives['opening']
+    return time * opening
 
-# Step 3: Write the sorted data back to a JSON file
-# sorted_data_dict = {k: v for k, v in sorted_data}
-
-# with open(save_path, 'w') as file:
-#     json.dump(sorted_data_dict, file, indent=4)
+sorted_data = sorted(data.items(), key=lambda x: sorting_function(x[1]))
 
 
-a = list(sorted_data)[0][1]["params"]
-params = []
+a = list(sorted_data)[0][1]["objectives"]
 
-# print(a)
-for b in a.values():
-    params.append(b)
+print (a)
 
-print(params)
+# params = []
+
+# for b in a.values():
+#     params.append(b)
+
+# print (params)
 
 # sorted_data_dict = {k: v for k, v in sorted_data[0]}
 
